3b61f86237a61849a69e8c066688e1b234c4c2a7
pat killean <pat.killean@gmail.com>
2025-08-21 17:05:42 -0400

    WIP sync-storage-test

--- test-clojure/me/tonsky/persistent_sorted_set/test/sync_storage_tests.cljs
+++ test-clojure/me/tonsky/persistent_sorted_set/test/sync_storage_tests.cljs
@@ -4,7 +4,6 @@
             [me.tonsky.persistent-sorted-set :as set]
             [me.tonsky.persistent-sorted-set.test.async-utils :as utils]))
 
-
 (deftest sync-storage-test
   (testing "Full integration test with sync storage - 10k elements"
     (let [storage (utils/make-sync-storage)
@@ -30,43 +29,40 @@
             (is (some? store-info))
             (is (map? store-info))
             (is (:root-address store-info))
-            (testing  "Check storage size"
-              (let [storage-data @(:*store storage)]
-                (is (> (count storage-data) 100) "Should have many nodes stored"))))
-
-           ;; Restore from storage
-           ; (let [restored (set/restore store-info storage)]
-           ;   (is (= n (count restored)))
-           ;
-           ;   ;; Verify data integrity
-           ;   (is (= 0 (first restored)))
-           ;   (is (= 9999 (last restored)))
-           ;
-           ;   ;; Check a sample of elements using contains?
-           ;   (doseq [i (range 0 10000 1000)]
-           ;     (is (contains? restored i) (str "Missing element: " i)))
-           ;
-           ;   ;; Verify slices on restored set
-           ;   (is (= (range 100) (vec (set/slice restored 0 99))))
-           ;   (is (= (range 5000 5100) (vec (set/slice restored 5000 5099))))
-           ;   (is (= (range 9900 10000) (vec (set/slice restored 9900 9999))))
-           ;
-           ;   ;; Test operations on restored set
-           ;   (testing "conj on restored set"
-           ;     (let [restored-with-new (set/conj restored 10000)]
-           ;       (is (= (inc n) (count restored-with-new)))
-           ;       (is (contains? restored-with-new 10000))))
-           ;
-           ;   ;; Test removing from restored set
-           ;   (testing "disj on restored set"
-           ;     (let [restored-without (set/disj restored 5000)]
-           ;       (is (= (dec n) (count restored-without)))
-           ;       (is (not (contains? restored-without 5000)))))
-           ;
-           ;   ;; Test iteration over restored set
-           ;   (testing "iteration over restored set"
-           ;     (let [first-100 (take 100 restored)]
-           ;       (is (= (range 100) first-100)))
-           ;     (let [last-100 (take 100 (drop 9900 restored))]
-           ;       (is (= (range 9900 10000) last-100)))))
-           ))))))
+            (and
+             (testing  "Check storage size"
+               (let [storage-data @(:*store storage)]
+                 (is (> (count storage-data) 100) "Should have many nodes stored")))
+             (testing "Restore from storage"
+               (let [restored (set/restore store-info storage)]
+                 (and
+                  (is (= n (count restored)))
+                  (is (= 0 (first restored)))
+                  (is (= 9999 (last restored)))
+                  (testing "Check a sample of elements using contains?"
+                    (loop [indices (range 0 10000 1000)]
+                      (let [i (first indices)]
+                        (if (nil? i)
+                          true
+                          (if-not (is (contains? restored i) (str "Missing element: " i))
+                            false
+                            (recur (rest indices)))))))
+                  (testing "Verify slices on restored set"
+                    (and
+                     (is (= (range 100) (vec (set/slice restored 0 99))))
+                     (is (= (range 5000 5100) (vec (set/slice restored 5000 5099))))
+                     (is (= (range 9900 10000) (vec (set/slice restored 9900 9999))))))
+                  (testing "conj on restored set"
+                    (let [restored-with-new (set/conj restored 10000)]
+                      (and
+                       (is (= (inc n) (count restored-with-new)))
+                       (is (contains? restored-with-new 10000)))))
+                  (testing "disj on restored set"
+                    (let [restored-without (set/disj restored 5000)]
+                      (and
+                       (is (= (dec n) (count restored-without)))
+                       (is (not (contains? restored-without 5000))))))
+                  (testing "iteration over restored set"
+                    (and
+                     (is (= (range 100) (take 100 restored)))
+                     (is (= (range 9900 10000) (take 100 (drop 9900 restored)))))))))))))))))
